8ad5a8ee29be0130fc6509debda7786fe25c32da
GOKUL N R <tl24btcs0001@vidyaacademy.ac.in>
2026-06-28 16:15:04 +0530

    Configure GitHub Pages deployment via Actions

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/LabSphere---Virtual-Science-Lab/',
   plugins: [
     react(),
     tailwindcss()
